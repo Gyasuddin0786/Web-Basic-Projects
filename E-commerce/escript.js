38473fadd523438aa562ccf9a169048d867f0da8
Gyasuddin <gyasuddinansari7854@gmail.com>
2025-01-20 18:43:27 +0530

    E-commerce frontend

---
+++ E-commerce/escript.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$(".wish-icon i").click(function(){
+		$(this).toggleClass("fa-heart fa-heart-o");
+	});
+});	
+
+
+// foooter css start from here
+
+$(document).ready(function(){
+    $('.third-row  li.active-1').find('span').css('color' , 'white');
+    $(' .third-row li').click(function() {
+    $(' .third-row li.active-1').find('span').css('color' , 'black');
+    $(' .third-row li.active-1').removeClass('active-1');
+    $(' .third-row li.active-1').addClass('text-dark');
+    $(this).addClass('active-1');
+    $(' .third-row  li.active-1').find('span').css('color' , 'white');
+});
+
+});
